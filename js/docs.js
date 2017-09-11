73738d4c0e3c9aab08efb4ba83ae5358d6bfde63
Tieson Trowbridge <tiesont@gmail.com>
2017-09-11 01:13:51 -0400

    Added popup for download button
    On the off-chance that someone actually visits the gh-pages for this repo, added a dialog for the download button (until I can build out redistributables).

--- js/docs.js
+++ js/docs.js
@@ -16,6 +16,17 @@ $(function () {
 
     try {
         window.prettyPrint && prettyPrint();
+        
+        $('#download-bootbox').on('click', function(e){
+            e.preventDefault();
+
+            bootbox.alert({
+                title: 'Bootbox.js',
+                message: '<h3><i class="fa fa-smile-o"></i> Hey there!</h3> <p>Thank you for your interest. Unfortunately, Bootbox 5 is not ready for distribution yet. ' 
+                    + 'If you want to start testing it, grab <a href="https://github.com/tiesont/bootbox">the source code</a> and ' 
+                    + 'report any issues you find.</p>'
+            });
+        });
     }
     catch (ex) {
         console.log(ex.message);
